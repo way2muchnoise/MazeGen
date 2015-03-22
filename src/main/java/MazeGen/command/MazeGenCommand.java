809cbe0d5d51bf5e9c58f864f72e4c8b1177967d
way2muchnoise <jens.baetens@live.be>
2015-03-22 18:18:38 +0100

    beef out the command a bit more

--- src/main/java/MazeGen/command/MazeGenCommand.java
+++ src/main/java/MazeGen/command/MazeGenCommand.java
@@ -29,21 +29,38 @@ public class MazeGenCommand extends CommandBase
     @Override
     public String getCommandUsage(ICommandSender sender)
     {
-        return "mazegen <width> <height>";
+        return "mazegen [<X> <Y> <Z>] <width> <height>";
     }
 
     @Override
     public void processCommand(ICommandSender sender, String[] args)
     {
-        if( args.length != 2)
+        int x, y, z;
+        int width, height;
+        if(args.length == 2)
+        {
+            x = sender.getPlayerCoordinates().posX;
+            y = sender.getPlayerCoordinates().posY;
+            z = sender.getPlayerCoordinates().posZ;
+            width = parseInt(sender, args[0]);
+            height = parseInt(sender, args[1]);
+        }
+        else if (args.length == 5)
+        {
+            x = parseInt(sender, args[0]);
+            y = parseInt(sender, args[1]);
+            z = parseInt(sender, args[2]);
+            width = parseInt(sender, args[3]);
+            height = parseInt(sender, args[4]);
+        }
+        else
         {
             throw new SyntaxErrorException();
         }
-        int width = parseInt(sender, args[0]);
-        int height = parseInt(sender, args[1]);
+
         MazeGenerator mazeGenerator = new MazeGenerator(width, height);
         mazeGenerator.gen();
         MazeDrawer mazeDrawer = new MazeDrawer(mazeGenerator.maze());
-        mazeDrawer.drawMaze(sender.getPlayerCoordinates().posX, sender.getPlayerCoordinates().posY, sender.getPlayerCoordinates().posZ, sender.getEntityWorld());
+        mazeDrawer.drawMaze(x, y, z, sender.getEntityWorld());
     }
 }
